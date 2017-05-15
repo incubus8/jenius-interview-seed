c691d4726125ebd21a5fb0fb21fccc5664b90e8e
rheza <rheza.satria.ta@gmail.com>
2017-05-15 11:32:37 +0700

    Initial commit

---
+++ test/services/contact.spec.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+const ContactService = require('../../app/api/contact/services/contact');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('contact service ', () => {
+  let sinonSandbox;
+  let mockServices;
+
+  beforeEach(() => {
+    mockServices = { logger: { info: () => {} } };
+    sinonSandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sinonSandbox.restore();
+    mockServices = null;
+  });
+
+  it('should throw error when Logger object is missing', () => {
+    mockServices = { };
+    expect(() => new ContactService(mockServices)).to.throw(Error, /Missing parameter: logger/);
+  });
+
+  it('should resolve promise after successfully getting contacts', (done) => {
+    expect(new ContactService(mockServices).getAll()).to.be.fulfilled.and.notify(done);
+  });
+});
